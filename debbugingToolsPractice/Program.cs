0e1dcc3ed5dbb0cb6ec757258d1b8076acb7c6ff
FerSanchez95 <fersanchezz95@gmail.com>
2025-02-08 11:32:58 -0300

    Se agregan archivos de practicas anteriores.

---
+++ debbugingToolsPractice/Program.cs
@@ -0,0 +1,15 @@
+﻿/*  
+This code instantiates a value and then calls the ChangeValue method
+to update the value. The code then prints the updated value to the console.
+*/
+int x = 5;
+
+int y = ChangeValue(x);
+
+Console.WriteLine(y);
+
+int ChangeValue(int value) 
+{
+    value = 10;
+    return value;
+}
