730dd975e7d28f194b7546575b366256cc8e5f6e
FerSanchez95 <fersanchezz95@gmail.com>
2025-02-09 20:21:10 -0300

    Se agraga un ejercicio de excepciones

--- Exception101/Program.cs
+++ Exception101/Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Exception101
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var primerEjercicio = new Ejercicio01();
+            primerEjercicio.Iniciar();
+        }
+    }
+}
+
+
+
+// // inputValues is used to store numeric values entered by a user
+// string[] inputValues = new string[]{"three", "9999999999", "0", "2" };
+
+// foreach (string inputValue in inputValues)
+// {
+//     int numValue = 0;
+//     try
+//     {
+//         numValue = int.Parse(inputValue);
+//     }
+//     catch (FormatException)
+//     {
+//         Console.WriteLine("Invalid readResult. Please enter a valid number.");
+//     }
+//     catch (OverflowException)
+//     {
+//         Console.WriteLine("The number you entered is too large or too small.");
+//     }
+//     catch(Exception ex)
+//     {
+//         Console.WriteLine(ex.Message);
+//     }
+// }
+
+// try-catch and exception usage explanation.
+// try
+// {
+//     Process1();
+// }
+// catch
+// {
+//     Console.WriteLine("An exception has occurred");
+// }
+
+// Console.WriteLine("Exit program");
+
+// static void Process1()
+// {
+//     try{
+//         WriteMessage();
+//     }
+//     catch (DivideByZeroException ex)
+//     {
+//         Console.WriteLine($"Exception cought in Process1: {ex.Message}");
+//     }
+
+// }
+// static void WriteMessage()
+// {
+//     double float1 = 3000.0;
+//     double float2 = 0.0;
+//     int number1 = 3000;
+//     int number2 = 0;
+//     byte smallNumber;
+
+//     Console.WriteLine(float1 / float2);
+//     Console.WriteLine(number1 / number2);
+//     checked
+//     {
+//         smallNumber = (byte)number1;
+//     }
+// }
